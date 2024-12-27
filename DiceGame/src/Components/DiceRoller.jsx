c51b1137661cb87f4d2b92aacb5daa2fc2cdb762
Manish Shrestha <manishshtestha472@gmail.com>
2024-12-27 23:44:15 +0545

    refactor DiceRoller component; simplify dice rolling logic and improve random number generation

--- DiceGame/src/Components/DiceRoller.jsx
+++ DiceGame/src/Components/DiceRoller.jsx
@@ -1,20 +1,17 @@
 import { useState } from "react";
-import dice1 from "../assets/dice1.png"
-import dice2 from "../assets/dice2.png"
-import dice3 from "../assets/dice3.png"
-import dice4 from "../assets/dice4.png"
-import dice5 from "../assets/dice5.png"
-import dice6 from "../assets/dice6.png"
 
 function DiceRoller(){
     const {diceValue,setDiceValue} = useState(1);
-    const handleRollClick = ()=>{
-        setDiceValue(Math.floor(Math.random()*6)+1);
-        console.log(diceValue);
+    const generateRandomNumber = (min, max)=>{
+        return Math.floor(Math.random()*(max-min+1))+min;
+    }
+
+    const Rollin=()=>{
+        setDiceValue(generateRandomNumber(1,7));
     }
     return(
-        <div className="w-32 h-32" onClick={handleRollClick}>
-            <img src={dice1} alt="Dice_1" className="pointer"/>
+        <div className="w-32 h-32" onClick={Rollin}>
+            <img src={`../assets/dice${diceValue}.png`} alt="Dice_1" className="pointer"/>
         </div>
     )
 }
